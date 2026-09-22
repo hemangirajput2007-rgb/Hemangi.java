0c68e4fc3b61a98b15fbfea1f52a1c574c7c69ba
hemangirajput2007-rgb <hemangirajput2007@gmail.com>
2026-09-22 00:18:50 -0700

    Add files via upload

---
+++ OperatorsDemo.java
@@ -0,0 +1,26 @@
+public class OperatorsDemo {
+    void add(int a,int b){;
+    int sum = a + b; 
+    System.out.println("Addition:"+sum);
+}
+
+int multiply(int a, int b){
+    return a * b ;
+}
+public static void main(String[]args){
+    int x =10, y = 3;
+    System.out.println("x + y =" + (x + y));
+    System.out.println("x - y =" +(x - y));
+    System.out.println("x * y =" +(x * y));
+    System.out.println("x % y =" +(x % y));
+
+    byte a = 10, b = 20;
+    int result = a+b;
+    System.out.println("Arithmetic Promotion Result:" + result);
+
+    OperatorsDemo obj = new OperatorsDemo();
+    obj.add(5, 7);
+    int product = obj.multiply(4, 6);
+    System.out.println("Multiplication: " + product);
+}
+}
